7680f14f1b2cad1d06b271ea423b81a4cf361e40
Tabata Ruiz <tabata.ruiz@rhsoftware.com.br>
2020-11-04 18:25:13 -0300

    Relacionamento nos models

--- src/database/migrations/1604421215205-CreateAppointments.ts
+++ src/database/migrations/1604421215205-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604421215205
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1604522098686-CreateUsers.ts
+++ src/database/migrations/1604522098686-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1604522098686 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
